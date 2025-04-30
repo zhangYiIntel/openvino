ead09e33e0b6ed9cce961846ada55b26f02dc19e
Zhang Yi <yi3.zhang@intel.com>
2025-04-30 14:00:14 +0800

    tempory ba

--- src/plugins/intel_cpu/src/nodes/kernels/scaled_attn/attn_quant.hpp
+++ src/plugins/intel_cpu/src/nodes/kernels/scaled_attn/attn_quant.hpp
@@ -11,6 +11,16 @@
 #include "openvino/core/type/element_type.hpp"
 #include "utils/plain_tensor.hpp"
 
+namespace ov::Extensions::Cpu {
+struct QuantizeParam {
+    bool quant_key_by_channel;
+    bool quant_value_by_channel;
+    size_t key_group_size;
+    size_t value_group_size;
+    bool is_sage_attn;
+};
+}
+
 namespace ov::Extensions::Cpu::XARCH {
 
 void attn_quantkv(const ov::intel_cpu::PlainTensor& k_src,
@@ -35,14 +45,11 @@ void paged_attn_quantkv(const ov::intel_cpu::PlainTensor& k_src,
                         const ov::intel_cpu::PlainTensor& block_indices_begins,
                         const ov::intel_cpu::PlainTensor& slot_mapping,
                         ov::intel_cpu::PlainTensor& temp_buffer,
-                        const bool quant_key_by_channel,
-                        const bool quant_value_by_channel,
-                        const size_t key_group_size,
-                        const size_t value_group_size);
+                        const QuantizeParam& quant_param);
 
 void attn_quant_u8(const float* src, uint8_t* dst, size_t n, float& scale, float& zp);
 
-void attn_dequant_u8(const uint8_t* src, float* dst, size_t n, float scale, float zp);
+void attn_dequant_u8(const uint8_t* src, float* dst, size_t n, float* params);
 
 void attn_quant_by_channel_u8(const float* src,
                               uint8_t* dst,
