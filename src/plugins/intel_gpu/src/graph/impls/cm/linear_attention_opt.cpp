4390b352f4e1fd1dbf081a13e5b961594656404e
Zhang Yi <yi3.zhang@intel.com>
2026-01-07 15:35:21 +0800

    add gpu primitive

---
+++ src/plugins/intel_gpu/src/graph/impls/cm/linear_attention_opt.cpp
@@ -0,0 +1,128 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+#include "vl_sdpa_opt.hpp"
+
+#include <algorithm>
+#include <cmath>
+
+#include "common_utils/kernel_generator_base.hpp"
+#include "intel_gpu/primitives/linear_attention.hpp"
+#include "primitive_cm_base.hpp"
+#include "primitive_inst.h"
+#include "registry/implementation_manager.hpp"
+#include "utils/kernel_generator.hpp"
+#include "linear_attention_opt.hpp"
+
+namespace ov::intel_gpu::cm {
+namespace {
+
+constexpr auto get_linear_attention_build_options() {
+    return " -cmc -Qxcm_register_file_size=256";
+}
+
+class LinearAttentionGenerator : public KernelGenerator {
+public:
+    LinearAttentionGenerator() : KernelGenerator("recurrent_gated_delta_rule") {}
+
+protected:
+    [[nodiscard]] std::string get_build_options(const RuntimeParams& params) const override {
+        return KernelGenerator::get_build_options(params) + get_linear_attention_build_options();
+    }
+
+    [[nodiscard]] JitConstants get_jit_constants(const RuntimeParams& params) const override {
+        auto jit = KernelGenerator::get_jit_constants(params);
+
+        auto desc = params.typed_desc<linear_attention>();
+        const auto query_shape = params.get_input_layout(0).get_partial_shape();
+        const auto key_shape = params.get_input_layout(1).get_partial_shape();
+        const auto value_shape = params.get_input_layout(2).get_partial_shape();
+
+        const size_t k_head_size = key_shape[query_shape.size() - 1].get_length();
+        const size_t v_head_size = value_shape[query_shape.size() - 1].get_length();
+        const size_t num_k_heads = query_shape[query_shape.size() - 3].get_length();
+        const size_t num_v_heads = value_shape[key_shape.size() - 3].get_length();
+        const float scale_factor = 1.0 / std::sqrt(static_cast<double>(k_head_size));
+
+        GPU_DEBUG_TRACE_DETAIL << "LinearAttention query_shape " << query_shape << ", key_shape " << key_shape << ", k_head_size="
+                               << k_head_size << ", v_head_size=" << v_head_size << ", num_k_heads=" << num_k_heads << ", num_v_heads=" << num_v_heads << '\n';
+
+        jit.add({
+            make_jit_constant("KERNEL_NAME", get_entry_point(params)),
+            make_jit_constant("K_HEAD_NUMS", num_k_heads),
+            make_jit_constant("V_HEAD_NUMS", num_v_heads),
+            make_jit_constant("K_HEAD_DIMS", k_head_size),
+            make_jit_constant("V_HEAD_DIMS", num_v_heads),
+            make_jit_constant("SCALE_FACTOR", scale_factor),
+        });
+
+        return jit;
+    }
+
+    [[nodiscard]] Arguments get_arguments_desc(const RuntimeParams& params) const override {
+        Arguments args;
+
+        for (uint32_t i = 0; i < params.input_layouts.size() - 1; i++) {  // inputs: q, k, v
+            args.push_back({ArgumentDescriptor::Types::INPUT, i});
+        }
+
+        for (uint32_t i = 0; i < params.output_layouts.size(); i++) {
+            args.push_back({ArgumentDescriptor::Types::OUTPUT, i});
+        }
+
+        return args;
+    }
+
+    [[nodiscard]] DispatchDataFunc get_dispatch_data_func() const override {
+        return DispatchDataFunc{[](const RuntimeParams& params, KernelData& kd, ImplRuntimeParams* rt_params) {
+            assert(!params.is_dynamic());
+            auto desc = params.typed_desc<linear_attention>();
+            const auto query_shape = params.get_input_layout(0).get_shape();
+            // B, T, H, K
+            const size_t batch = query_shape[0];
+            const size_t head_nums = query_shape[2];
+
+            auto& wgs = kd.params.workGroups;
+            wgs.global = {batch, head_nums, 1};
+            wgs.local = {1, 1, 8};
+        }};
+    }
+};
+
+class LinearAttentionOptImpl : public PrimitiveImplCM {
+public:
+    DECLARE_OBJECT_TYPE_SERIALIZATION(ov::intel_gpu::cm::LinearAttentionOptImpl)
+
+    Stage::Ptr linear_attn = make_stage<LinearAttentionGenerator>();
+
+    LinearAttentionOptImpl() : PrimitiveImplOCL(LinearAttentionOptImplementationManager::get_type_info_static()) {
+    }
+    LinearAttentionOptImpl(const program_node& node, const RuntimeParams& params) : LinearAttentionOptImpl() {
+        add_stage(linear_attn, params);
+    }
+
+    [[nodiscard]] std::unique_ptr<primitive_impl> clone() const override {
+        return make_deep_copy<LinearAttentionOptImpl>(this);
+    }
+
+    void update_rt_params(const cldnn::primitive_inst& instance) override {
+        update_stages_flags(instance);
+        return;
+    }
+
+    cldnn::event::ptr execute(const std::vector<cldnn::event::ptr>& events, cldnn::primitive_inst& instance) override {
+        return PrimitiveImplCM::execute(events, instance);
+    }
+};
+
+}  // namespace
+
+std::unique_ptr<primitive_impl> LinearAttentionOptImplementationManager::create_impl(const program_node& node, const RuntimeParams& params) const {
+    assert(node.is_type<linear_attention>());
+    return std::make_unique<LinearAttentionOptImpl>(node, params);
+}
+
+}  // namespace ov::intel_gpu::cm
+
+BIND_BINARY_BUFFER_WITH_TYPE(cldnn::linear_attention)
+BIND_BINARY_BUFFER_WITH_TYPE(ov::intel_gpu::cm::LinearAttentionOptImpl)
---
+++ src/plugins/intel_gpu/src/graph/linear_attention.cpp
@@ -0,0 +1,54 @@
+// Copyright (C) 2025 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "linear_attention_inst.h"
+#include "primitive_type_base.h"
+#include "json_object.h"
+#include "to_string_utils.h"
+#include <string>
+#include <vector>
+
+namespace cldnn {
+GPU_DEFINE_PRIMITIVE_TYPE_ID(linear_attention)
+
+layout linear_attention_inst::calc_output_layout(linear_attention_node const& node, kernel_impl_params const& impl_param) {
+    return calc_output_layouts<ov::PartialShape>(node, impl_param)[0];
+}
+
+template<typename ShapeType>
+std::vector<layout> linear_attention_inst::calc_output_layouts(linear_attention_node const& node, const kernel_impl_params& impl_param) {
+    const auto& desc = impl_param.typed_desc<linear_attention>();
+    const auto& all_inputs = node.get_input_layouts();
+    if (all_inputs.size() != 6)
+        OPENVINO_THROW("linear_attention's must have 6 inputs");
+    // query, key, value, g, beta, initial_states
+    auto value_layout = impl_param.get_input_layout(2);
+    auto states_layout = impl_param.get_input_layout(5);
+    auto output_layout = layout{value_layout.get_partial_shape(), value_layout.data_type, value_layout.format};
+    return {output_layout, states_layout};
+}
+
+template std::vector<layout> linear_attention_inst::calc_output_layouts<ov::PartialShape>(linear_attention_node const& node, const kernel_impl_params& impl_param);
+
+std::string linear_attention_inst::to_string(linear_attention_node const& node) {
+    auto node_info = node.desc_to_json();
+    auto desc = node.get_primitive();
+
+    std::stringstream primitive_description;
+
+    json_composite linear_attention_info;
+    linear_attention_info.add("query", node.input(0).id());
+    linear_attention_info.add("key", node.input(1).id());
+    linear_attention_info.add("value", node.input(2).id());
+    linear_attention_info.add("g", node.input(3).id());
+    linear_attention_info.add("beta", node.input(3).id());
+
+    node_info->add("linear_attention_info", linear_attention_info);
+    node_info->dump(primitive_description);
+
+    return primitive_description.str();
+}
+
+linear_attention_inst::typed_primitive_inst(network& network, linear_attention_node const& node) : parent(network, node) { }
+}  // namespace cldnn
