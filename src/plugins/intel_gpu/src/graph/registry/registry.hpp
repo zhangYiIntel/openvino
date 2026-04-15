f643a5dcc1621aa6a28a319e39bcd8e43e1401d4
Zhang Yi <yi3.zhang@intel.com>
2026-04-15 19:08:00 +0800

    fix missing pa_kv_reorder

--- src/plugins/intel_gpu/src/graph/registry/registry.hpp
+++ src/plugins/intel_gpu/src/graph/registry/registry.hpp
@@ -150,6 +150,7 @@ REGISTER_IMPLS(lstm_cell);
 REGISTER_IMPLS(lstm_seq);
 REGISTER_IMPLS(gru_seq);
 REGISTER_IMPLS(non_max_suppression);
+REGISTER_IMPLS(pa_kv_reorder);
 REGISTER_IMPLS(paged_attention);
 REGISTER_IMPLS(paged_causal_conv1d);
 REGISTER_IMPLS(paged_gated_delta_net);
